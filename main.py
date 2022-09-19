192751100f2e6b2de3bdd12e5af19ab598c75537
pigeoninatrenchcoat <113436554+pigeoninatrenchcoat@users.noreply.github.com>
2022-09-19 20:41:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ win = tk.Tk()
 canvas = tk.Canvas(win, height = 450, width = 600, bg= "black" )
 canvas.pack()
 
+
 voda = []
 zem = []
-
+mom_blue = True
 pic_zem = tk.PhotoImage(file = "images\ostrov0.png")
 pic_voda = tk.PhotoImage(file = "images\ostrov3.png")
+b_kruh = tk.PhotoImage(file = "images\ostrov_kruh0.png")
+h_kruh = tk.PhotoImage(file = "images\ostrov_kruh1.png")
 
+def callback():
+    print("yup")
+
+def zmen_kruh():
+    global kruh, mom_blue
+    if mom_blue == True:
+        kruh.config(image= h_kruh)
+        mom_blue = False
+    else:
+        kruh.config(image=b_kruh)
+        mom_blue = True
+
+kruh = tk.Button(image=b_kruh, command=zmen_kruh, borderwidth=0)
 
 def make_a_scene():
     global zem
@@ -24,5 +40,8 @@ def make_a_scene():
             else:
                 temp = canvas.create_image(50 * riadok, 50*stlp, anchor= "nw", image = pic_voda)
                 voda.append(temp)
+    kruh.place(x=500, y=100)
+canvas.bind("<Button-1>", callback())
+
 make_a_scene()
 win.mainloop()
